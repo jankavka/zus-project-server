835fdc6cae55eefd2d8a4eceadbd6c0a0100cb3b
jankavka <jankavka@seznam.cz>
2025-03-14 22:26:52 +0100

    added annotation RequestBody to create method in ArticleController

--- src/main/java/cz/kavka/controller/ArticleController.java
+++ src/main/java/cz/kavka/controller/ArticleController.java
@@ -4,6 +4,7 @@ import cz.kavka.dto.ArticleDTO;
 import cz.kavka.service.serviceInterface.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class ArticleController {
     }
 
     @PostMapping("/create")
-    public ArticleDTO create(ArticleDTO articleDTO){
+    public ArticleDTO create(@RequestBody ArticleDTO articleDTO){
         return articleService.create(articleDTO);
     }
 }
